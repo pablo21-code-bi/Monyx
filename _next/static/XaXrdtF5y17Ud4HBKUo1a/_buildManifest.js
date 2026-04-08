34d206fc2f4594107bbf59377f7dfe52972ff3fa
Pablo <pablo21-code-bi@users.noreply.github.com>
2026-04-08 20:40:44 -0300

    Updates

--- _next/static/XaXrdtF5y17Ud4HBKUo1a/_buildManifest.js
+++ _next/static/XaXrdtF5y17Ud4HBKUo1a/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/Monyx//_next/:path+",
-        "destination": "/Monyx/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
